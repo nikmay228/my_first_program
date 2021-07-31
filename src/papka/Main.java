7b05cded38328a3d14f79a737570eaf993bc0668
nikmay228 <nananaga@rambler.ru>
2021-07-31 12:53:11 +0300

    dobavili file main

---
+++ src/papka/Main.java
@@ -0,0 +1,17 @@
+package papka;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input namber1: ");
+        int namber1 = in.nextInt();
+        System.out.print("Input namber2: ");
+        int namber2 = in.nextInt();
+        int namber3 = namber1 + namber2;
+        System.out.printf("Namber3: %d", namber3);
+        in.close();
+    }
+}
