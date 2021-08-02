9f5975f6f7c7b409a2bf3b3b9bcc882a3599c875
Pavel Kavaliuk <pavel.kavaliuk@godel.first-utility.com>
2021-08-02 17:58:28 +0300

    Add example of decrement.

--- src/papka/Main.java
+++ src/papka/Main.java
@@ -1,13 +1,16 @@
 package papka;
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        int i = -8;
-//число -8 в двоичной системе
-        System.out.println(Integer.toBinaryString(i));
-//обратыный перевод из двоичной системы в десятичную
-        System.out.println(Integer.parseInt("11111111111111111111111111111000", 2));
+        decrementExample();
     }
+
+    static void decrementExample() {
+        int firstDigit = 4;
+
+        int result = --firstDigit;
+        System.out.printf("result=%d, firstFigit=%d", result, firstDigit);
+    }
+
 }
