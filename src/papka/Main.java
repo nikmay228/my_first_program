987846e5cc5cca4514f1551fef5f1b3d0b90c61e
nikmay228 <nananaga@rambler.ru>
2021-08-05 19:32:35 +0300

    норм?

--- src/papka/Main.java
+++ src/papka/Main.java
@@ -1,16 +1,22 @@
 package papka;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        decrementExample();
+    Scanner a = new Scanner(System.in);
+    int num1, num2;
+        System.out.println("Please, Enter namber1: ");
+        num1 = a.nextInt();
+        System.out.println("Please, Enter namber2: ");
+        num2 = a.nextInt();
+        if(num1==num2) {
+            System.out.println("Two numbers ravni");
+        }
+        if(num1<num2) {
+            System.out.println("Number1 menshe number2");
+        }
+        if(num1>num2){
+            System.out.println("Number1 bolshe numer2");
+        }
     }
-
-    static void decrementExample() {
-        int firstDigit = 4;
-
-        int result = --firstDigit;
-        System.out.printf("result=%d, firstFigit=%d", result, firstDigit);
-    }
-
 }
