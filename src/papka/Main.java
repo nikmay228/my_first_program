9f078f362ff378f618cd6de7abed7aeb7f279d2f
nikmay228 <nananaga@rambler.ru>
2021-08-07 15:23:33 +0300

    норм?

--- src/papka/Main.java
+++ src/papka/Main.java
@@ -1,22 +1,36 @@
 package papka;
+
 import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-    Scanner a = new Scanner(System.in);
-    int num1, num2;
-        System.out.println("Please, Enter namber1: ");
-        num1 = a.nextInt();
-        System.out.println("Please, Enter namber2: ");
-        num2 = a.nextInt();
-        if(num1==num2) {
-            System.out.println("Two numbers ravni");
-        }
-        if(num1<num2) {
-            System.out.println("Number1 menshe number2");
-        }
-        if(num1>num2){
-            System.out.println("Number1 bolshe numer2");
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        int num1 = in.nextInt();
+
+        System.out.print("Введите второе число: ");
+        int num2 = in.nextInt();
+
+        System.out.println("Введите номер операции: 1. Сложение 2. Вычитание 3. Умножение");
+        int num3 = in.nextInt();
+
+        switch (num3) {
+            case 1:
+                num3 = num1 + num2;
+                System.out.printf("Результат операции: %d", num3);
+                break;
+            case 2:
+                num3 = num1 - num2;
+                System.out.printf("Результат операции: %d", num3);
+                break;
+            case 3:
+                num3 = num1 * num2;
+                System.out.printf("Результат операции: %d", num3);
+                break;
+            default:
+                System.out.print("Операция не определена");
         }
     }
 }
