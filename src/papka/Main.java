4de9946c7bacd2ab15c4520336ce2dae9e4a6d74
Pavel Kavaliuk <pavel.kavaliuk@godel.first-utility.com>
2021-08-09 11:33:28 +0300

    Better now.

--- src/papka/Main.java
+++ src/papka/Main.java
@@ -3,34 +3,38 @@ package papka;
 import java.util.Scanner;
 
 public class Main {
+//  Константа на уровне класса
+    public static final String RESULT_PATTERN = "Результат операции: %d";
 
     public static void main(String[] args) {
+//        Константа на уровне метода. Можно использовать её, но лучше выносить константу на уровень класса
+//        final String RESULT_PATTERN = "Результат операции: %d";
 
         Scanner in = new Scanner(System.in);
         System.out.print("Введите первое число: ");
-        int num1 = in.nextInt();
+        final int num1 = in.nextInt();
 
         System.out.print("Введите второе число: ");
-        int num2 = in.nextInt();
+        final int num2 = in.nextInt();
 
         System.out.println("Введите номер операции: 1. Сложение 2. Вычитание 3. Умножение");
-        int num3 = in.nextInt();
+        int result;
 
-        switch (num3) {
+        switch (in.nextInt()) {
             case 1:
-                num3 = num1 + num2;
-                System.out.printf("Результат операции: %d", num3);
+                result = num1 + num2;
                 break;
             case 2:
-                num3 = num1 - num2;
-                System.out.printf("Результат операции: %d", num3);
+                result = num1 - num2;
                 break;
             case 3:
-                num3 = num1 * num2;
-                System.out.printf("Результат операции: %d", num3);
+                result = num1 * num2;
                 break;
             default:
                 System.out.print("Операция не определена");
+                return;
         }
+
+        System.out.printf(RESULT_PATTERN, result);
     }
 }
